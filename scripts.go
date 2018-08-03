5c697f03ac4202646ebafb349e8f3e75f9b41814
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2018-08-03 12:17:59 +0200

    use ARGV

--- scripts.go
+++ scripts.go
@@ -2,11 +2,11 @@ package redisqueue
 
 import "github.com/gomodule/redigo/redis"
 
-var popJobsScript = redis.NewScript(3, `
+var popJobsScript = redis.NewScript(1, `
 local key_queue = KEYS[1]
 local value_queue = key_queue .. ':values'
-local timestamp = KEYS[2]
-local limit = KEYS[3]
+local timestamp = ARGV[1]
+local limit = ARGV[2]
 local keys = redis.call('zrangebyscore', key_queue, '-inf', timestamp, 'LIMIT', 0, limit)
 if table.getn(keys) == 0 then
 	return {}
@@ -25,10 +25,10 @@ if redis.call('zadd', key_queue, job.when, job.id) ~= 1 then
 end
 return redis.call('hset', value_queue, job.id, job.body)`)
 
-var removeScript = redis.NewScript(2, `
+var removeScript = redis.NewScript(1, `
 local key_queue = KEYS[1]
 local value_queue = key_queue .. ':values'
-local key = KEYS[2]
+local key = ARGV[1]
 if redis.call('zrem', key_queue, key) ~= 1 then
 	return 0
 end
